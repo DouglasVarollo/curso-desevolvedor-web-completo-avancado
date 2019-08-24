dcd2ca61184917fc20f3d7519d470369330cd4f9
= <dcvarollo@gmail.com>
2019-08-23 22:13:44 -0300

    Exercicios do curso de Bootstrap

---
+++ 08_-_Bootstrap_Avançado/unidade10_Avançado/_js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+	$('.carousel').carousel({
+		interval:2000, // intervalo entre cada imagem
+		pause: "null", // quando o mouse estiver em cima, ele continua passando as imagens
+		wrap: false // para na ultima foto
+	})
+});
